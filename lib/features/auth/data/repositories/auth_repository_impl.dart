af3f90cc63d8afdedf01c826e8f87b34b120165b
Bilal Aksoy <sloeyeleet@gmail.com>
2024-06-20 19:12:49 +0300

    Implemented user representation

--- lib/features/auth/data/repositories/auth_repository_impl.dart
+++ lib/features/auth/data/repositories/auth_repository_impl.dart
@@ -1,6 +1,7 @@
 import 'package:blog_app/core/error/exceptions.dart';
 import 'package:blog_app/core/error/failures.dart';
 import 'package:blog_app/features/auth/data/datasources/auth_remote_data_source.dart';
+import 'package:blog_app/features/auth/domain/entities/user.dart';
 import 'package:blog_app/features/auth/domain/repository/auth_repository.dart';
 import 'package:fpdart/fpdart.dart';
 
@@ -10,7 +11,7 @@ class AuthRepositoryImpl implements AuthRepository {
   AuthRepositoryImpl(this._remoteDataSource);
 
   @override
-  Future<Either<Failure, String>> logInWithEmailAndPassword({
+  Future<Either<Failure, User>> logInWithEmailAndPassword({
     required String email,
     required String password,
   }) {
@@ -19,19 +20,19 @@ class AuthRepositoryImpl implements AuthRepository {
   }
 
   @override
-  Future<Either<Failure, String>> signUpWithEmailAndPassword({
+  Future<Either<Failure, User>> signUpWithEmailAndPassword({
     required String name,
     required String email,
     required String password,
   }) async {
     try {
-      final userId = await _remoteDataSource.signUpWithEmailAndPassword(
+      final user = await _remoteDataSource.signUpWithEmailAndPassword(
         name: name,
         email: email,
         password: password,
       );
 
-      return right(userId);
+      return right(user);
     } on ServerException catch (e) {
       return left(Failure(e.message));
     }
