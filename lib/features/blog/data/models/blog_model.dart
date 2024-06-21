1030990a82275491b10915f6378fb0d60527fa4a
Bilal Aksoy <sloeyeleet@gmail.com>
2024-06-21 15:47:12 +0300

    Implemented blog representation with data source

---
+++ lib/features/blog/data/models/blog_model.dart
@@ -0,0 +1,39 @@
+import 'package:blog_app/features/blog/domain/entities/blog.dart';
+
+class BlogModel extends Blog {
+  BlogModel({
+    required super.id,
+    required super.posterId,
+    required super.title,
+    required super.content,
+    required super.imageUrl,
+    required super.topics,
+    required super.updatedAt,
+  });
+
+  factory BlogModel.fromJson(Map<String, dynamic> json) {
+    return BlogModel(
+      id: json['id'],
+      posterId: json['poster_id'],
+      title: json['title'],
+      content: json['content'],
+      imageUrl: json['image_url'],
+      topics: List<String>.from(json['topics']),
+      updatedAt: json['updated_at'] != null
+          ? DateTime.parse(json['updated_at'])
+          : DateTime.now(),
+    );
+  }
+
+  Map<String, dynamic> toJson() {
+    return {
+      'id': super.id,
+      'poster_id': super.posterId,
+      'title': super.title,
+      'content': super.content,
+      'image_url': super.imageUrl,
+      'topics': super.topics,
+      'updated_at': super.updatedAt.toIso8601String(),
+    };
+  }
+}
